d5715354370f545c17b5cad956e093cb034bb1a2
Giovanni Di Santi <giovanni.disanti@protonmail.com>
2024-01-13 23:39:55 +0100

    Rename iterative characteristic model

--- eval_desv.py
+++ eval_desv.py
@@ -3,15 +3,14 @@ from cipher.des_vectorised import DESV
 from make_data import make_train_data, make_real_differences_data
 from eval import evaluate
 
-net_3_2 = load_model("./freshly_trained_nets/DESV_iterative_characteristic_3_rounds_permutations.h5")
-net_3_d2 = [0x19600000, 0x00000000]
 
 net_3_0 = load_model("./freshly_trained_nets/DESV_3_rounds_4008000004000000.h5")
 net_3_d0 = [0x40080000, 0x04000000]
 
+net_3_2 = load_model("./freshly_trained_nets/DESV_3_rounds_1960000004000000.h5")
+net_3_d2 = [0x19600000, 0x00000000]
+
 des = DESV(n_rounds=3)
-
-
 
 if __name__ == "__main__":
     print(f"Evaluation with ∆ = {list(map(hex, net_3_d0))}")
